67013f414f9b957df0eaa344e8bc50c12a500f76
Matt <voorhees.matthew@gmail.com>
2018-09-14 14:05:17 -0700

    Create User and sessions controller with routes and views

--- goal_app/spec/controllers/users_controller_spec.rb
+++ goal_app/spec/controllers/users_controller_spec.rb
@@ -12,13 +12,13 @@ RSpec.describe UsersController, type: :controller do
     context "with valid params" do
       it "logs user in" do
         #User.create(username: 'Matt' password: 'password')
-        post :create params: {user: {username: 'Matt', password: 'password'}}
+        post :create, params: {user: {username: 'Matt', password: 'password'}}
         user = User.find_by(username: 'Matt')
         expect(session[:session_token]).to eq(user.session_token)
       end
       
       it 'redirects to the user show page' do
-        post :create params: {user: {username: 'Matt', password: 'password'}}
+        post :create, params: {user: {username: 'Matt', password: 'password'}}
         user = User.find_by(username: 'Matt')
         expect(response).to redirect_to(user_url(user))
       end
@@ -26,7 +26,7 @@ RSpec.describe UsersController, type: :controller do
     
     context "with invalid params" do
       it 'validates the presence of password and renders show page with flash errors' do
-        post :create params: {user: {username: 'Matt'}}
+        post :create, params: {user: {username: 'Matt'}}
         expect(response).to render_template(:new)
         expect(flash[:errors]).to be_present
       end
